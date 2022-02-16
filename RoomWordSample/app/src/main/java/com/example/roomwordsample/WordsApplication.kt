2ffc17b043ab00d2c98159d1f0323ee4869f9119
mtnmr <mei.rila1013@gmail.com>
2022-02-16 17:04:29 +0900

    Roomdatabase change(WordDatabaseCallback)

--- RoomWordSample/app/src/main/java/com/example/roomwordsample/WordsApplication.kt
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordsApplication.kt
@@ -3,8 +3,12 @@ package com.example.roomwordsample
 import android.app.Application
 import com.example.roomwordsample.reposistory.WordRepository
 import com.example.roomwordsample.room.WordRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication:Application() {
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
--- RoomWordSample/app/src/main/java/com/example/roomwordsample/room/WordRoomDatabase.kt
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/room/WordRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
@@ -16,14 +19,40 @@ abstract class WordRoomDatabase : RoomDatabase() {
         private var INSTANCE : WordRoomDatabase? = null
 
         //最初にアクセスされたらデータベースを作成
-        fun getDatabase(context:Context):WordRoomDatabase{
+        //アプリ起動のたびにデータをリセットするためにRoomDatabase.Callbackを実装
+        //Roomの操作はコルーチンで行うため引数に追加（WordsApplicationにも）
+        fun getDatabase(context:Context, scope:CoroutineScope):WordRoomDatabase{
             return INSTANCE ?: synchronized(this){
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
-                    "word_database").build()
+                    "word_database")
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
+
                 INSTANCE = instance
                 return instance
+            }
+        }
+    }
+
+    ////アプリ起動のたびにデータをリセットするためにRoomDatabase.Callbackを実装
+    private class WordDatabaseCallback(private val scope: CoroutineScope)
+        :RoomDatabase.Callback(){
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let{ database ->
+                scope.launch {
+                    val wordDao = database.wordDao()
+                    wordDao.deleteAll()
+
+                    //sample wordを追加しておく
+                    var word = Word("Hello")
+                    wordDao.insert(word)
+                    word = Word("World")
+                    wordDao.insert(word)
+                }
             }
         }
     }
