bbcfe34234106fa2c66a5c95610d86c97267692d
mtnmr <mei.rila1013@gmail.com>
2022-02-26 16:58:41 +0900

    recyclerView add

--- inventory/app/src/main/java/com/example/inventory/fragment/ItemListFragment.kt
+++ inventory/app/src/main/java/com/example/inventory/fragment/ItemListFragment.kt
@@ -1,16 +1,23 @@
 package com.example.inventory.fragment
 
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.inventory.R
+import androidx.recyclerview.widget.ListAdapter
+import com.example.inventory.*
 import com.example.inventory.databinding.FragmentItemListBinding
 
 class ItemListFragment : Fragment() {
+
+    private val viewModel:InventoryViewModel by activityViewModels{
+        InventoryViewModelFactory((activity?.application as InventoryApplication).database.itemDao())
+    }
 
     private var _binding : FragmentItemListBinding? = null
     private val binding get() = _binding!!
@@ -25,6 +32,17 @@ class ItemListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ItemListAdapter{}
+        binding.recyclerView.adapter = adapter
+
+        viewModel.allItems.observe(this.viewLifecycleOwner){ items ->
+            items.let {
+                adapter.submitList(it)
+            }
+        }
+
+
         binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
 
         binding.floatingActionButton.setOnClickListener{
