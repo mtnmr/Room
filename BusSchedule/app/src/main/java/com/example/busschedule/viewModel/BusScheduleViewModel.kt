33d0a3785ea1c97dee16a6004dfc8d948c417851
mtnmr <mei.rila1013@gmail.com>
2022-03-25 16:02:48 +0900

    flow

--- BusSchedule/app/src/main/java/com/example/busschedule/viewModel/BusScheduleViewModel.kt
+++ BusSchedule/app/src/main/java/com/example/busschedule/viewModel/BusScheduleViewModel.kt
@@ -4,22 +4,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.Schedule
 import com.example.busschedule.database.ScheduleDao
+import kotlinx.coroutines.flow.Flow
 import java.lang.IllegalArgumentException
 
-class BusScheduleViewModel(private val scheduleDao:ScheduleDao): ViewModel(){
-    fun fullSchedule():List<Schedule> = scheduleDao.getAll()
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
 
-    fun scheduleForStopName(name:String):List<Schedule> = scheduleDao.getByStopName(name)
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 }
 
 class BusScheduleViewModelFactory(
-    private val scheduleDao:ScheduleDao
-    ) : ViewModelProvider.Factory{
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)){
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return BusScheduleViewModel(scheduleDao) as T
         }
-        throw (IllegalArgumentException("Unknown ViewModel class"))
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
